7fb258dc5f30d4bb2ddd46462c99ffe16e002b31
Alex R <alexis.rj2110@gmail.com>
2025-06-12 18:30:57 -0500

    V0.0.2

--- taller/deporteecuador/futbolec/admin.py
+++ taller/deporteecuador/futbolec/admin.py
@@ -1,36 +1,26 @@
 from django.contrib import admin
-
-# Importar las clases del modelo
-from administrativo.models import Estudiante, Modulo, Matricula
-from import_export.admin import ImportExportModelAdmin
-from import_export import resources
+from .models import Equipo, Jugador, Campeonato, CampeonatoEquipo
 
 
-class Equipo(ImportExportModelAdmin, admin.ModelAdmin):
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_equipo', 'siglas', 'username')
+    search_fields = ('nombre_equipo', 'siglas')
 
-    list_display = ('nombre', 'apellido', 'cedula', 'edad', 'tipo_estudiante')
-    search_fields = ('nombre', 'cedula')
-    exclude = ("modulos",) # se excluye de la interfaz del admin
 
-admin.site.register(Estudiante, EstudianteAdmin)
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ('nombre_jugador', 'posicion', 'numero', 'sueldo', 'equipo')
+    search_fields = ('nombre_jugador', 'posicion')
 
-class Jugador(ImportExportModelAdmin, admin.ModelAdmin):
 
-    list_display = ('estudiante', 'modulo', 'comentario')
-    search_fields = ('estudiante__nombre', 'modulo__nombre')
+@admin.register(Campeonato)
+class CampeonatoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_campeonato', 'nombre_auspiciante')
+    search_fields = ('nombre_campeonato', 'nombre_auspiciante')
 
-admin.site.register(Matricula, MatriculaAdmin)
 
-class Campeonato(ImportExportModelAdmin, admin.ModelAdmin):
-
-    list_display = ('estudiante', 'modulo', 'comentario')
-    search_fields = ('estudiante__nombre', 'modulo__nombre')
-
-admin.site.register(Matricula, MatriculaAdmin)
-
-class CampeonatoEquipo(ImportExportModelAdmin, admin.ModelAdmin):
-
-    list_display = ('estudiante', 'modulo', 'comentario')
-    search_fields = ('estudiante__nombre', 'modulo__nombre')
-
-admin.site.register(Matricula, MatriculaAdmin)
+@admin.register(CampeonatoEquipo)
+class CampeonatoEquipoAdmin(admin.ModelAdmin):
+    list_display = ('año', 'campeonato', 'equipo')
+    search_fields = ('año', 'campeonato__nombre_campeonato', 'equipo__nombre_equipo')
--- taller/deporteecuador/futbolec/models.py
+++ taller/deporteecuador/futbolec/models.py
@@ -4,10 +4,12 @@ from django.db import models
 # Create your models here.
 
 class Equipo(models.Model):
-
     nombre_equipo = models.CharField(max_length=100, unique=True)
     siglas = models.CharField(max_length=100, blank=True)  # el campo puede
     username = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.nombre_equipo} ({self.siglas})'
 
 
 class Jugador(models.Model):
@@ -17,15 +19,22 @@ class Jugador(models.Model):
     sueldo = models.DecimalField(decimal_places=2, max_digits=10)
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.nombre_jugador} - #{self.numero} ({self.posicion})'
+
 
 class Campeonato(models.Model):
-
     nombre_campeonato = models.CharField(max_length=100, unique=True)
     nombre_auspiciante = models.CharField(max_length=100, blank=True)
 
-class CampeonatoEquipo(models.Model):
+    def __str__(self):
+        return self.nombre_campeonato
 
+
+class CampeonatoEquipo(models.Model):
     año = models.CharField(max_length=30, blank=True)
     campeonato = models.ForeignKey(Campeonato, on_delete=models.CASCADE)
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.campeonato} - {self.equipo} ({self.año})'
